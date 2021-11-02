c7a438ff1929f0fdbc2d1aeb2787f0ed62e419ea
yanpeihao1998 <35412863+yanpeihao1998@users.noreply.github.com>
2021-11-02 17:43:49 +0800

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import smtplib
 from email.mime.text import MIMEText
 from email.header import Header
 
+
 signIn = {'username': os.environ["USERNAME"], #学号
           'password': os.environ["PASSWORD"]} #登陆密码
 
@@ -70,9 +71,9 @@ mail_user = "2819237058@qq.com"  # 用户名
 mail_pass = "lyimhkqaudcudfbd"  # 口令
 
 sender = '2819237058@qq.com'
-receivers = ['17863900870@163.com']  # 接收邮件，可设置为你的QQ邮箱或者其他邮箱
+receivers = ['s20070011@s.upc.edu.cn']  # 接收邮件，可设置为你的QQ邮箱或者其他邮箱
 
-message = MIMEText(saveJson['m'], 'plain', 'utf-8') 
+message = MIMEText(saveJson['m'], 'plain', 'utf-8')
 message['From'] = Header("疫情防控通填报通知", 'utf-8')
 message['To'] = Header("测试", 'utf-8')
 
